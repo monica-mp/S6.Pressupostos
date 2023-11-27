d7164f12c555df19067a6ecd4bc2d5c5c72ab2a1
Monica <mmunzp@gmail.com>
2023-11-27 12:31:21 +0100

    N2. Exercici 8

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,6 +9,7 @@ function TodoList() {
   const [todos, setTodos] = useState([]);
 
   const [orderBy, setOrderBy] = useState(null);
+  const [searchName, setSearchName] = useState("");
 
   const calculateTotalForServices = () => {
     return arrayServices.reduce((total, service) => {
@@ -23,7 +24,6 @@ function TodoList() {
     return services.web ? (webOptions.pages + webOptions.languages) * 30 : 0;
   };
 
- 
   const addTodo = (todo) => {
     const selectedServices = arrayServices
       .filter((service) => services && services[service.name.toLowerCase()])
@@ -41,44 +41,51 @@ function TodoList() {
       wPages: webOptions.pages,
       wLanguages: webOptions.languages,
       total: totalServices + totalWeb,
-      date: new Date()
+      date: new Date(),
     };
- 
-    
+
     const newTodos = [newTodo, ...todos];
-    setTodos(newTodos); 
-    
+    setTodos(newTodos);
   };
 
-
   const handleOrderBy = (type) => {
-      
     setOrderBy(type);
-   
+
     if (type === "name") {
       setTodos([...todos].sort((a, b) => a.name.localeCompare(b.name)));
     } else if (type === "date") {
       setTodos([...todos].sort((a, b) => new Date(b.date) - new Date(a.date)));
-    } else if(type === "import"){
-      setTodos([...todos].sort((a, b) => b.total - a.total ))
+    } else if (type === "import") {
+      setTodos([...todos].sort((a, b) => b.total - a.total));
     }
   };
 
+  const filterTodos = () => {
+    return todos.filter((todo) =>
+      todo.name.toLowerCase().includes(searchName.toLowerCase())
+    );
+  };
 
   return (
     <div>
-      
       <TodoForm onSubmit={addTodo} />
       <div className="mx-auto w-3/6 pt-8 border-t-2 border-dashed border-teal-900">
         <h1 className="text-xl">Pressupostos en curs:</h1>
         <div className="flex gap-4 justify-end font-bold">
-        <button onClick={() => handleOrderBy("name")}>Nom</button>
-        <button onClick={() => handleOrderBy("date")}>Data</button>   
-        <button onClick={() => handleOrderBy("import")}>Import 🡫</button>     
-        </div>        
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered input-info input-sm"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+          />
+          <button onClick={() => handleOrderBy("name")}>Nom</button>
+          <button onClick={() => handleOrderBy("date")}>Data</button>
+          <button onClick={() => handleOrderBy("import")}>Import 🡫</button>
+        </div>
       </div>
-      
-      <Todo todos={todos} arrayServices={arrayServices} />
+
+      <Todo todos={filterTodos()} arrayServices={arrayServices} />
     </div>
   );
 }
