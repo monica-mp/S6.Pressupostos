0fcd4066316d0f351c6249884e73bfec69c94211
Monica <mmunzp@gmail.com>
2023-11-27 10:33:40 +0100

    N2. Exercici 6

--- src/components/WebOptions.jsx
+++ src/components/WebOptions.jsx
@@ -3,9 +3,43 @@ import { useCheckbox } from "../CheckboxContext";
 function WebOptions() {
   const { webOptions, onPagesChange, onLanguagesChange } = useCheckbox();
   return (
-    <div className="flex flex-col mt-8 border-2 rounded-lg border-cyan-600 p-2 self-end w-80 gap-4 ">
-      <div className="flex justify-between">
-        <span>Número de pàgines:</span>
+    <div className="flex flex-col mt-8 border-2 rounded-lg border-cyan-600 p-2 self-end w-96 gap-4 ">
+      <div className="flex">
+        <div>
+          <button
+            className="btn btn-sm btn-ghost p-0 mr-2"
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="stroke-current shrink-0 w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </button>
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <form method="dialog">
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 className="font-bold text-lg">Número de pàgines</h3>
+              <p className="py-4">
+                Afegeix les pàgines que tindrà el teu projecte. El cost de cada
+                pàgina es de 30&euro;
+              </p>
+            </div>
+          </dialog>
+        </div>
+        <span className="self-center mr-auto">Número de pàgines:</span>
         <div>
           <button
             className="btn btn-circle btn-xs border-cyan-700"
@@ -30,8 +64,43 @@ function WebOptions() {
           </button>
         </div>
       </div>
-      <div className="flex justify-between">
-        <span>Número de llenguatges:</span>
+
+      <div className="flex">
+        <div>
+          <button
+            className="btn btn-sm btn-ghost p-0 mr-2"
+            onClick={() => document.getElementById("my_modal_2").showModal()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="stroke-current shrink-0 w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </button>
+          <dialog id="my_modal_2" className="modal">
+            <div className="modal-box">
+              <form method="dialog">
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 className="font-bold text-lg">Número de llenguatges</h3>
+              <p className="py-4">
+                Afegeix els llenguatges que tindrà el teu projecte. El cost de
+                cada llenguatge es de 30&euro;
+              </p>
+            </div>
+          </dialog>
+        </div>
+        <span className="self-center mr-auto">Número de llenguatges:</span>
         <div>
           <button
             className="btn btn-circle btn-xs border-cyan-700"
